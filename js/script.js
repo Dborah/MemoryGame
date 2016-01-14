be2a97aa3c933752280176abcb40ea83b22f1638
Dborah <deborahferreirafonseca@gmail.com>
2016-01-14 19:58:18 -0200

    imagens da fase easy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function mDown(obj) {
     obj.style.backgroundColor = "#FFFFFF";
+    imagens = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 
-    obj.src = "img/2.jpg";
+    obj.src = imagens[Math.floor(Math.random() * 3)];
     
 }
