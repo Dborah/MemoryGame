03203715c032d81aecc03c58779e82029d011543
Dborah <deborahferreirafonseca@gmail.com>
2016-01-16 23:23:23 -0200

    correção de erros

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function mDown(obj) {
     obj.style.backgroundColor = "#FFFFFF";
+    
+
     imagens = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
 
-    obj.src = imagens[Math.floor(Math.random() * 3)];
-    
+    obj.src = imagens[Math.floor(Math.random() * 3)];    
 }
