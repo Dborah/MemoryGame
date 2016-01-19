ff1a300cf4a503124dc5145ac66ef7b0c7fe86c3
Déborah Fonseca <deborahferreirafonseca@gmail.com>
2016-01-19 17:48:37 -0200

    Delete script.js

--- js/script.js
+++
@@ -1,5 +0,0 @@
-function mDown(obj) {
-    obj.style.backgroundColor = "#FFFFFF";
-    imagens = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
-    obj.src = imagens[Math.floor(Math.random() * 3)];    
-}
