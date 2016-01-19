6262e6fde0023c9fbbfbfa8a5da533c1a5b60e20
Dborah <deborahferreirafonseca@gmail.com>
2016-01-18 23:17:46 -0200

    imagens do jogo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 function mDown(obj) {
     obj.style.backgroundColor = "#FFFFFF";
-    
-
     imagens = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
-
     obj.src = imagens[Math.floor(Math.random() * 3)];    
 }
